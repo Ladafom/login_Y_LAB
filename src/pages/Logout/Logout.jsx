e16dbe305482a0d83672d7aa2417ae5bd0e1cde2
ladafmn <ladafmn@gmail.com>
2024-09-05 15:45:16 +0700

    style:  redesign

--- src/pages/Logout/Logout.jsx
+++ src/pages/Logout/Logout.jsx
@@ -1,22 +1,24 @@
-import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/context";
+import './logout.css'
 
 function Logout() {
 
-  const navigate = useNavigate()
+  const {isAuthorised, setIsAuthorised} = useContext(AuthContext)
 
   function onLogout(e){
     e.preventDefault()
     localStorage.removeItem('token')
-    navigate('/login')
+    setIsAuthorised(false)
   }
 
   return (
-    <>
+    <div className="logout">
       <h1>Welcome!</h1>
       <form onSubmit={onLogout} >
         <button>Logout</button>
       </form>
-    </>
+    </div>
   );
 }
 
