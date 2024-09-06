f1e09c4369b07d8c1b1dac05d561a6f4ede9b067
ladafmn <ladafmn@gmail.com>
2024-09-06 12:20:26 +0700

    style: add adaptive

--- src/utils/errorMessages.js
+++ src/utils/errorMessages.js
@@ -1,5 +1,5 @@
 export const errorMessages = {
   patternMismatch: 'Введен неверный формат почты',
-  tooShort: 'Пароль должен содержать минимум 7 символов',
+  tooShort: 'Mинимум 7 символов',
   valueMissing: 'Поле обязательно для заполнения'
 }
